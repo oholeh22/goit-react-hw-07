3f1f6402c6688f48ed9b01dbef3dd5a4960851ff
Oleh Shyian <shiyan4242@gmail.com>
2024-12-06 13:14:56 +0100

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,15 @@
 import './App.css'
+import ContactForm from './ContactForm/ContactForm'
+import ContactList from './ContactList/ContactList'
+import SearchBox from './SearchBox/SearchBox'
 
 function App() {
     return (
         <div className='container'>
             <h1>Phonebook</h1>
+                <ContactForm />
+                <SearchBox />
+                <ContactList />
         </div>
         
     )
